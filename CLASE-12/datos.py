d20c7b8265027f48cf4bd8ac291c419ba39409ca
Pablobur1 <134552895+Pablobur1@users.noreply.github.com>
2023-05-24 16:43:22 -0400

    Datos Solicitados

---
+++ CLASE-12/datos.py
@@ -0,0 +1,10 @@
+# EJEMPLO DE USO DE GITHUB
+print("INGRESO DE DATOS")
+print("----------------")
+vNom = input("Ingrese su nombre: ")
+vEdad = input("Ingrese su edad: ")
+print("--------------------------")
+print(f"Su Nombre es: {vNom}")
+print(f"Su Edad es: {vEdad}")
+print("Programa Finalizado")
+
