588543ad850e4fb286fe27a17c7e0df2ff58b88d
Pablobur1 <134552895+Pablobur1@users.noreply.github.com>
2023-05-24 17:04:52 -0400

    Datos Validados en Archivo

--- CLASE-12/datos.py
+++ CLASE-12/datos.py
@@ -2,7 +2,13 @@
 print("INGRESO DE DATOS")
 print("----------------")
 vNom = input("Ingrese su nombre: ")
-vEdad = input("Ingrese su edad: ")
+while True:
+    try:
+     vEdad =int(input("Ingrese su edad: "))
+    except:
+     print("Error de Ingreso")
+
+
 print("--------------------------")
 print(f"Su Nombre es: {vNom}")
 print(f"Su Edad es: {vEdad}")
